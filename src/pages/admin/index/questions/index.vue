58662ac9c1fa81e9e500657d5927f3a4d5854319
Duch Chamroeun <chamroeurn@soramitsukhmer.com>
2025-05-29 11:33:59 +0700

    feat: remove unused edit and delete functionality from QuestionViewModel and related components

--- src/pages/admin/index/questions/index.vue
+++ src/pages/admin/index/questions/index.vue
@@ -3,9 +3,6 @@
     <template #title>
       Question List
     </template>
-    <template #action>
-      Create Question
-    </template>
     <template #content>
       <v-data-table-server
         key="tblquestionlist"
@@ -30,86 +27,9 @@
             >
               <v-icon>mdi-eye</v-icon>
             </v-btn>
-            <v-btn
-              class="mr-2"
-              color="primary"
-              icon
-              size="small"
-              title="Edit Question"
-              @click="questionViewModel.openEditDialog(item)"
-            >
-              <v-icon>mdi-pencil</v-icon>
-            </v-btn>
-            <v-btn
-              color="error"
-              icon
-              size="small"
-              title="Delete Question"
-              @click="questionViewModel.openDeleteDialog(item)"
-            >
-              <v-icon>mdi-delete</v-icon>
-            </v-btn>
           </div>
         </template>
       </v-data-table-server>
-    </template>
-    <template #dialogs>
-      <v-dialog v-model="questionViewModel.model.deleteDialog.show" max-width="500">
-        <v-card>
-          <v-card-title>Confirm Deletion</v-card-title>
-          <v-card-text>
-            Are you sure you want to delete question: <strong>{{ questionViewModel.model.deleteDialog.question?.questionText }}</strong>?
-            This action cannot be undone.
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer />
-            <v-btn color="grey" @click="questionViewModel.closeDeleteDialog">Cancel</v-btn>
-            <v-btn color="red" :loading="questionViewModel.model.deleteDialog.loading" @click="questionViewModel.confirmDeleteQuestion">
-              <span v-if="questionViewModel.model.deleteDialog.loading">Deleting...</span>
-              <span v-else>Delete</span>
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-
-      <v-dialog v-model="questionViewModel.model.editDialog.show" max-width="500">
-        <v-card>
-          <v-card-title>
-            {{ questionViewModel.model.editMode === 'create' ? 'Create Question' : 'Edit Question' }}
-          </v-card-title>
-          <v-card-text>
-            <v-form :ref="questionViewModel.editForm" v-model="questionViewModel.model.editFormValid">
-              <v-text-field
-                v-model="questionViewModel.model.editDialog.question.questionText"
-                label="Question Text"
-                required
-                :rules="questionViewModel.questionTextRules"
-              />
-              <v-text-field
-                v-model="questionViewModel.model.editDialog.question.time"
-                label="Time Limit (seconds)"
-                min="1"
-                required
-                :rules="questionViewModel.timeRules"
-                type="number"
-              />
-            </v-form>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer />
-            <v-btn color="grey" @click="questionViewModel.closeEditDialog">Cancel</v-btn>
-            <v-btn
-              color="blue"
-              :disabled="!questionViewModel.model.editFormValid"
-              :loading="questionViewModel.model.editDialog.loading"
-              @click="questionViewModel.saveEditQuestion"
-            >
-              <span v-if="questionViewModel.model.editDialog.loading">Saving...</span>
-              <span v-else>Save</span>
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
     </template>
   </main-content-list>
 </template>
--- src/viewmodel/question.ts
+++ src/viewmodel/question.ts
@@ -4,8 +4,7 @@ import type { TableOption } from '@/models/table';
 import router from '@/router';
 import { questionService } from '@/services/question';
 import { simulateDelay } from '@/utils/delay';
-import { reactive, ref } from 'vue';
-import type { VForm } from 'vuetify/components';
+import { reactive } from 'vue';
 
 class QuestionViewModel {
 
@@ -16,39 +15,12 @@ class QuestionViewModel {
     { title: 'Actions', key: 'actions', sortable: false, width: '10%' },
   ];
 
-  readonly questionTextRules = [
-    (v: string) => !!v || 'Question Text is required',
-    (v: string) => v.length >= 5 || 'Question Text must be at least 5 characters',
-  ];
-  readonly timeRules = [
-    (v: number) => !!v || 'Time Limit is required',
-    (v: number) => v > 0 || 'Time Limit must be greater than 0',
-  ];
-
-  editForm = ref<VForm | null>(null);
-
   readonly model = reactive({
     loading: false,
     questions: [] as Question[],
     totalQuestions: 0,
     itemsPerPage: 10,
-    editFormValid: false,
-    editMode: 'edit' as 'edit' | 'create',
-    editDialog: {
-      show: false,
-      question: {} as Question,
-      loading: false,
-    },
-    deleteDialog: {
-      show: false,
-      question: null as Question | null,
-      loading: false,
-    },
   })
-
-  get tableLoading (): boolean {
-    return this.model.loading;
-  }
 
   readonly fetchQuestions = async (option: TableOption) => {
     this.model.loading = true;
@@ -64,86 +36,6 @@ class QuestionViewModel {
       }
     }
   }
-
-  readonly openDeleteDialog = (question: Question) => {
-    this.model.deleteDialog = {
-      show: true,
-      question,
-      loading: false,
-    };
-  };
-
-  readonly closeDeleteDialog = () => {
-    this.model.deleteDialog.show = false;
-    this.model.deleteDialog.question = null;
-  };
-
-  readonly confirmDeleteQuestion = () => {
-    if (!this.model.deleteDialog.question) return;
-
-    this.model.deleteDialog.loading = true;
-    // Simulate delete API call
-    setTimeout(() => {
-      // In a real application, you would call your delete API here
-      console.log('Deleting question:', this.model.deleteDialog.question);
-      // Remove the question from the list
-      this.model.questions = this.model.questions.filter(q => q.id !== this.model.deleteDialog.question!.id);
-      this.model.deleteDialog.loading = false;
-      this.closeDeleteDialog();
-    }, 500);
-  };
-
-  readonly openEditDialog = (question: Question) => {
-    this.model.editMode = 'edit';
-    this.model.editDialog = {
-      show: true,
-      question: { ...question },
-      loading: false,
-    };
-  };
-
-  readonly openCreateDialog = () => {
-    this.model.editMode = 'create';
-    this.model.editDialog = {
-      show: true,
-      question: {
-        id: '',
-        questionText: '',
-        time: 60, // Default time
-      },
-      loading: false,
-    };
-  };
-
-  readonly closeEditDialog = () => {
-    this.model.editDialog.show = false;
-    this.model.editDialog.question = {} as Question;
-  };
-
-  readonly saveEditQuestion = () => {
-    if (!this.editForm.value?.validate()) return;
-
-    this.model.editDialog.loading = true;
-    // Simulate save API call
-    setTimeout(() => {
-      // In a real application, you would call your update API here
-      console.log('Saving question:', this.model.editDialog.question);
-      if (this.model.editMode === 'edit') {
-        this.model.questions = this.model.questions.map(q =>
-          q.id === this.model.editDialog.question.id ? { ...this.model.editDialog.question } : q
-        );
-      } else {
-        const newQuestion = {
-          ...this.model.editDialog.question,
-          id: crypto.randomUUID(),
-        };
-        this.model.questions.push(newQuestion);
-      }
-
-      this.model.editDialog.loading = false;
-      this.closeEditDialog();
-    }, 500);
-  };
 
   readonly viewQuestionDetail = (question: Question) => {
     router.push('/admin/questions/' + question.id);
